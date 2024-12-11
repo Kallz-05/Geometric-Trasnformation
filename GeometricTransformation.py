f152c591dbd1524ad73ea3ae52f7dfa842d6136e
Kallz-05 <fikryhaikall07@gmail.com>
2024-12-11 16:50:16 +0700

    Update GeometricTransformation.py

--- GeometricTransformation.py
+++ GeometricTransformation.py
@@ -1,25 +1,3 @@
-import streamlit as st
-from PIL import Image
-import cv2
-import numpy as np
-
-
-
-select=st.selectbox("pick",["camera","upload image"])
-if select=="camera":
-    cam=st.camera_input("capture a photo")
-    if cam is not None:
-        file_bytes = np.asarray(bytearray(cam.read()), dtype=np.uint8)
-        opencv_image = cv2.imdecode(file_bytes, 1)
-        st.image(opencv_image,channel="RGB")
-
-else:
-    uploaded_file = st.file_uploader("Choose a image file", type=["jpg","png"])
-    if uploaded_file is not None:
-        file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
-        opencv_image1= cv2.imdecode(file_bytes, 1)
-        st.image(opencv_image1,channels="BGR")
-
 import cv2
 import numpy as np
 import os
